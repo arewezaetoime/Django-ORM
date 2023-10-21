5ee4ea6164aa570937197f64982e015f0f57aa5d
hristoiliev <hristoiliev.1994@gmail.com>
2023-10-22 00:12:27 +0300

    ss
    ss

--- 2. Django Models Basics/orm_skeleton_lab_02/main_app/models.py
+++ 2. Django Models Basics/orm_skeleton_lab_02/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 
@@ -30,5 +32,13 @@ class Department(models.Model):
     last_edited_on = models.DateTimeField(auto_now=True, editable=False)
 
 
+class Project(models.Model):
 
-
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+    budget = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    duration_in_days = models.PositiveIntegerField(null=True, blank=True, verbose_name="Duration in Days")
+    estimated_hours = models.FloatField()
+    start_date = models.DateField(default=date.today())
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    last_edited_on = models.DateTimeField(auto_now=True, editable=False)
