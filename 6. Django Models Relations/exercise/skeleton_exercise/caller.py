035171136c04579c50961a7f851b51ef1154cde5
hristoiliev <hristoiliev.1994@gmail.com>
2023-11-10 19:26:50 +0200

    labs continue

--- 6. Django Models Relations/exercise/skeleton_exercise/caller.py
+++ 6. Django Models Relations/exercise/skeleton_exercise/caller.py
@@ -6,5 +6,49 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Author, Book, Artist, Song
+
 
 # Create queries within functions
+
+def show_all_authors_with_their_books() -> str:
+    authors_with_books = []
+
+    authors = Author.objects.all().order_by('id')  #a better way to do it is with prefetch
+
+    for author in authors:
+        books = Book.objects.filter(author=author)
+
+        if not books:
+            continue
+
+        titles = ', '.join(b.title for b in books)
+        authors_with_books.append(f'{author.name} has written - {titles}!')
+
+    return '\n'.join(authors_with_books)
+
+
+def delete_all_authors_without_books() -> None:
+    Author.objects.filter(book__isnull=True).delete()
+
+
+def add_song_to_artist(artist_name: str, song_title: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.add(song)
+
+
+def get_songs_by_artist(artist_name: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+
+    return artist.songs.all().order_by('-id')
+
+
+def remove_song_from_artist(artist_name: str, song_title: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.remove(song)
+
+
--- 6. Django Models Relations/exercise/skeleton_exercise/main_app/models.py
+++ 6. Django Models Relations/exercise/skeleton_exercise/main_app/models.py
@@ -2,3 +2,66 @@ from django.db import models
 
 # Create your models here.
 
+
+class Author(models.Model):
+    name = models.CharField(
+        max_length=40,
+    )
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=40,
+    )
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
+
+    author = models.ForeignKey(
+        to=Author,
+        on_delete=models.CASCADE,
+    )
+
+
+class Song(models.Model):
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+
+class Artist(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    songs = models.ManyToManyField(
+        to=Song,
+        related_name="artists"
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+
+class Review(models.Model):
+    description = models.TextField(
+        max_length=200,
+    )
+
+    rating = models.PositiveIntegerField()
+
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="reviews"
+    )
