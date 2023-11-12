a17f918999773b1694040ce86c7bc4305cad9ce0
hristoiliev <hristoiliev.1994@gmail.com>
2023-11-12 11:32:29 +0200

    labs

--- 6. Django Models Relations/exercise/skeleton_exercise/caller.py
+++ 6. Django Models Relations/exercise/skeleton_exercise/caller.py
@@ -1,12 +1,16 @@
 import os
+from datetime import timedelta, date
+
 import django
+from django.db.models import QuerySet
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Author, Book, Artist, Song
+from main_app.models import Author, Book, Artist, Song, Product, Review, DrivingLicense, Driver, Owner, Registration, \
+    Car
 
 
 # Create queries within functions
@@ -52,3 +56,64 @@ def remove_song_from_artist(artist_name: str, song_title: str) -> None:
     artist.songs.remove(song)
 
 
+def calculate_average_rating_for_product_by_name(product_name: str) -> float:
+    product = Product.objects.get(name=product_name)
+    reviews = product.reviews.all()
+
+    total_rating = sum(r.rating for r in reviews)
+    average_rating = total_rating / len(reviews)
+
+    return average_rating
+
+    # better way
+    # product = Product.objects.annotate(
+    #     total_ratings=Sum('review__rating'),
+    #     num_reviews=Count('review')
+    # ).get(name=product_name)
+    #
+    # average_rating = product.total_ratings / product.num_reviews
+    #
+    # return average_rating
+
+
+def get_reviews_with_high_ratings(threshold: int) -> QuerySet[Review]:
+    return Review.objects.filter(rating__gte=threshold)
+
+
+def get_products_with_no_reviews() -> QuerySet[Product]:
+    return Product.objects.filter(reviews__isnull=True).order_by('-name')
+
+
+def delete_products_without_reviews() -> None:
+    Product.objects.filter(reviews__isnull=True).delete()
+
+
+def calculate_licenses_expiration_dates() -> str:
+    licenses = DrivingLicense.objects.order_by('-license_number')
+
+    return '\n'.join(str(l) for l in licenses)
+
+
+def get_drivers_with_expired_licenses(due_date: date) -> QuerySet[Driver]:
+    expiration_cutoff_date = due_date - timedelta(days=365)
+
+    expired_drivers = Driver.objects.filter(drivinglicense__issue_date__gt=expiration_cutoff_date)
+
+    return expired_drivers
+
+
+def register_car_by_owner(owner: Owner) -> str:
+    registration = Registration.objects.filter(car__isnull=True).first()
+    car = Car.objects.filter(registration__isnull=True, owner=owner).first()
+
+    car.owner = owner
+    car.registration = registration
+
+    car.save()
+
+    registration.registration_date = date.today()
+    registration.car = car
+
+    registration.save()
+
+    return f"Successfully registered {car.model} to {owner.name} with registration number {registration.registration_number}."
--- 6. Django Models Relations/exercise/skeleton_exercise/main_app/models.py
+++ 6. Django Models Relations/exercise/skeleton_exercise/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db import models
 
 # Create your models here.
@@ -64,4 +66,70 @@ class Review(models.Model):
         null=True,
         blank=True,
         related_name="reviews"
+    )
+
+
+class Driver(models.Model):
+    first_name = models.CharField(
+        max_length=50,
+    )
+    last_name = models.CharField(
+        max_length=50,
+    )
+
+
+class DrivingLicense(models.Model):
+    license_number = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+    issue_date = models.DateField()
+    driver = models.OneToOneField(
+        to=Driver,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        expiration_date = self.issue_date + timedelta(days=365)
+        return f"License with id: {self.license_number} expires on {expiration_date}!"
+
+
+class Owner(models.Model):
+    name = models.CharField(
+        max_length=50,
+    )
+
+
+class Car(models.Model):
+    model = models.CharField(
+        max_length=50,
+    )
+
+    year = models.PositiveIntegerField()
+
+    owner = models.ForeignKey(
+        to=Owner,
+        on_delete=models.CASCADE,
+        related_name='cars',
+        null=True,
+        blank=True,
+    )
+
+
+class Registration(models.Model):
+    registration_number = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+
+    registration_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    car = models.OneToOneField(
+        to=Car,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
