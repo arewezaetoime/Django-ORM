aa2797d5a6aaf00eb893433dab1470553d8d7db0
hristoiliev <hristoiliev.1994@gmail.com>
2023-11-23 13:45:50 +0200

    advanced models techiques lab

--- 8. Advanced Django Model Techniques/Exercise 08/skeleton_exercise_08/caller.py
+++ 8. Advanced Django Model Techniques/Exercise 08/skeleton_exercise_08/caller.py
@@ -7,30 +7,3 @@ from django.core.exceptions import ValidationError
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Customer
-# Import your models here
-
-# Create queries within functions
-customer = Customer(
-
-name="Svetlin Nakov1",
-
-age=1,
-
-email="nakov@example",
-
-phone_number="+35912345678",
-
-website_url="htsatps://nakov.com/"
-
-)
-
-try:
-
-	customer.full_clean()
-
-	customer.save()
-
-except ValidationError as e:
-
-	print('\n'.join(e.messages))
--- 8. Advanced Django Model Techniques/Exercise 08/skeleton_exercise_08/main_app/validators.py
+++ 8. Advanced Django Model Techniques/Exercise 08/skeleton_exercise_08/main_app/validators.py
@@ -3,17 +3,12 @@ import re
 from django.core.exceptions import ValidationError
 
 
-# def validate_name(value):
-# 	if not all(value.split().isalpha()) or not all(value.split().isspace()):
-# 		return ValidationError('"Name can only contain letters and spaces".')
-
-
-def validate_name_v2(value):
-	for char in value:
-		if not (char.isalpha() or char.isspace()):
-			raise ValidationError("Name can only contain letters and spaces")
+def validate_name(value):
+    for char in value:
+        if not (char.isalpha() or char.isspace()):
+            raise ValidationError("Name can only contain letters and spaces")
 
 
 def validate_phone_number(value):
-	if not re.match(r'^\+359\d{9}&', value):
-		raise ValidationError("Phone number must start with a '+359' followed by 9 digits")
+    if not re.match(r'^\+359\d{9}$', value):
+        raise ValidationError("Phone number must start with a '+359' followed by 9 digits")
